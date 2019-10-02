beb450dc617fdb9dd8fa9f5f9db50208097a4a26
stepgazaille <stepgazaille@gmail.com>
2019-10-02 12:04:03 -0400

    implement LEAD-3 algorithm

--- resources/lead3.py
+++ resources/lead3.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from nltk.tokenize import sent_tokenize
 
 class Lead3(Resource):
 
@@ -12,8 +13,10 @@ class Lead3(Resource):
 
     def post(self):
         doc = Lead3.parser.parse_args()['doc']
+        sentences = sent_tokenize(doc)
+        summary = " ".join(sentences[:3])
         try:
-            return {"summary": doc[:11]}
+            return {"summary": summary}
         except Exception as e:
             print(str(e))
             return {'message': 'There was a problem summarizing the document'}, 404
